b6edf96649194d5643e32288bbc6e68ac8f957d2
Ostindev229 <ostindev229@gmail.com>
2023-07-12 13:35:37 +0200

    Ajout du dossier 'dist' pour le déploiement avec le publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const publicPath = process.env.NODE_ENV === 'production' ? '/TaskMasters/' : '/'
+module.exports = {
+ publicPath: publicPath,
+}
